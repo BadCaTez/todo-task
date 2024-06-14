6aee1c1d58f6a84728c7bf69e67dcd2bee1e0930
BadCaTez <ivan.ru928@gmail.com>
2024-06-14 17:58:37 +0200

    modified:   src/App.vue 	modified:   src/main.css 	modified:   src/main.js

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,51 @@
 <template>
-  <main class = "app">
+  <v-app>
 
-    <section class="gretting_text">
+    <v-app-bar color="blue">
+      <h1>Мой ToDo лист</h1>
+    </v-app-bar>
+
+  <v-container class = "app" >
+
+    <v-col class="gretting_text">
       <h3 class="title">Мой ToDo лист для WordPress</h3>
-    </section>
+    </v-col>
 
-    <div class="input-section">
-      <section class="create-todo">
-        <form @submit.prevent="addTodo">
+    <v-row class="input-section" >
+      <v-col class="create-todo">
+        <v-form @submit.prevent="addTodo">
           <h3>Какие планы на ближайшее время?</h3>
-          <input
-          type="text"
-          placeholder="например: купить молоко"
-          v-model="text"
-          />
+          <v-text-field placeholder="например: купить молоко" v-model="text"/>
+          <v-btn variant="tonal" color="blue" type="submit">Добавить</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
 
-          <input type="submit" value="Добавить"/>
-        </form>
-      </section>
-    </div>
-
-    <div class="todo-section">
-      <section class="todo-list">
+    <v-list class="todo-section" lines="one">
+      <v-col class="todo-list">
         <h2 v-show="todos.length === 0">Нету дел на сегодня</h2>
 
-        <div class="list">
-        <div v-for="todo in todos"
+        <v-sheet class="d-flex flex-warp bg-surface-variant">
+        <v-sheet v-for="todo in todos" class="ma-2 pa-2"
         :class="`todo-item ${todo.done && 'done'}`"
         :key="todo.id"
         >
-        <label>
-          <input type="checkbox" v-model="todo.done">
-        </label>
+        <v-label>
+          <v-checkbox type="checkbox" v-model="todo.done"/>
+        </v-label>
 
-        <div class="todo-content">
-          <input type="text" v-model="todo.todo"/>
-        </div>
-        <div class="actions">
-          <v-button class="delete" @click="deleteTodo(todo)">Удалить</v-button>
-        </div>
-      </div>
-    </div>
-  </section>
-</div>
-</main>
+        <v-row class="todo-content">
+          <v-text-field type="text" variant="plain" v-model="todo.todo"/>
+        </v-row>
+        <v-row class="actions">
+          <v-button variant="tonal" color="red" @click="deleteTodo(todo)">Удалить</v-button>
+        </v-row>
+      </v-sheet>
+    </v-sheet>
+  </v-col>
+</v-list>
+  </v-container>
+</v-app>
 </template>
 
 <script setup>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import './/style.css'
-import './/main.css'
 
 loadFonts()
 
